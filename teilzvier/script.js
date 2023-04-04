251226045a0915aaf8ee891d8173a8ef79b67067
TwinMine <129844817+TwinMine@users.noreply.github.com>
2023-04-04 11:50:52 +0200

    Add files via upload

---
+++ teilzvier/script.js
@@ -0,0 +1,10 @@
+function changeColor(){
+    console.log('Bitte anklicken')
+
+    let meineSektion =  document.querySelector('.right')
+
+    console.log(meineSektion)
+
+    meineSektion.style.backgroundColor = 'orange'
+
+}
