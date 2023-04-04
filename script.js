db6c9ccf7e8f5616930226ef3188b552a9688bab
TwinMine <129844817+TwinMine@users.noreply.github.com>
2023-04-04 11:40:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeColor(){
+    console.log('Schwieriges Thema')
+
+    let meineSektion =  document.querySelector('.least')
+
+    console.log(meineSektion)
+
+    meineSektion.style.backgroundColor = 'pink'
+
+}
